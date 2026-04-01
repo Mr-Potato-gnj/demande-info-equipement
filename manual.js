e87dcff1bca658cbaa07baf53366578e4f4116da
Mr-Potato-gnj <charles.lavigne5@gmail.com>
2026-04-01 14:18:54 -0400

    Add manuals data for Caterpillar and JLG models

---
+++ manual.js
@@ -0,0 +1,12 @@
+const manuals = [
+  {
+    brand: "Caterpillar",
+    model: "320D",
+    path: "\\\\owncloud\\Manuels\\Caterpillar\\320D.pdf"
+  },
+  {
+    brand: "JLG",
+    model: "600AJ",
+    path: "\\\\owncloud\\Manuels\\JLG\\600AJ.pdf"
+  }
+];
